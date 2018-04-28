9faa17148d2b4289440ab829d3c853ba878e8ee9
mikelagoma <mike.lagoma@gmail.com>
2018-04-28 09:34:40 -0700

    add html files to render, start with login page

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 // //cloud database
